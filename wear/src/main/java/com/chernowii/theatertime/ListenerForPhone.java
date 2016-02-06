6beaca531715d83918491fcc90af32c60bd58068
Konrad Iturbe <github@chernowii.com>
2016-02-06 02:45:03 +0100

    This works, and thats it for 2AM!

--- wear/src/main/java/com/chernowii/theatertime/ListenerForPhone.java
+++ wear/src/main/java/com/chernowii/theatertime/ListenerForPhone.java
@@ -39,29 +39,7 @@ public class ListenerForPhone extends WearableListenerService {
             try {
                 Process su = Runtime.getRuntime().exec("su");
                 DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
-                outputStream.writeBytes("input tap 100 100;");
-                outputStream.flush();
-                outputStream.writeBytes("exit\n");
-                outputStream.flush();
-                su.waitFor();
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-            try {
-                Process su = Runtime.getRuntime().exec("su");
-                DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
-                outputStream.writeBytes("input tap 100 100;");
-                outputStream.flush();
-                outputStream.writeBytes("exit\n");
-                outputStream.flush();
-                su.waitFor();
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-            try {
-                Process su = Runtime.getRuntime().exec("su");
-                DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
-                outputStream.writeBytes("input tap 100 100;");
+                outputStream.writeBytes("input keyevent 4\n input keyevent 26\n");
                 outputStream.flush();
                 outputStream.writeBytes("exit\n");
                 outputStream.flush();
