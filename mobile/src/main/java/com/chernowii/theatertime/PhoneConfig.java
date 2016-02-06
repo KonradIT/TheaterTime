bba9a322793b3f8e1619e8b56f8d52329ff6bd7c
Konrad Iturbe <github@chernowii.com>
2016-02-06 23:29:35 +0100

    Thats it for today, AlarmManager does not seem to work though

--- mobile/src/main/java/com/chernowii/theatertime/PhoneConfig.java
+++ mobile/src/main/java/com/chernowii/theatertime/PhoneConfig.java
@@ -1,6 +1,12 @@
 package com.chernowii.theatertime;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -29,9 +35,15 @@ public class PhoneConfig extends AppCompatActivity implements GoogleApiClient.Co
     private static final String off = "/off";
     public String START_TIME = "00:00";
     public String STOP_TIME = "00:00";
+    public int START_TIME_HH = 00;
+    public int START_TIME_MM = 00;
+    public int STOP_TIME_HH = 00;
+    public int STOP_TIME_MM = 00;
     Node wearNode;
     GoogleApiClient wearGoogleApiClient;
     private boolean mResolvingError=false;
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -136,6 +148,8 @@ public class PhoneConfig extends AppCompatActivity implements GoogleApiClient.Co
             @Override
             public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                 START_TIME = selectedHour + ":" + selectedMinute;
+                START_TIME_HH = selectedHour;
+                START_TIME_MM = selectedMinute;
                 TextView startTime = (TextView)findViewById(R.id.startTime);
                 startTime.setText(START_TIME);
             }
@@ -153,12 +167,30 @@ public class PhoneConfig extends AppCompatActivity implements GoogleApiClient.Co
             @Override
             public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                 STOP_TIME = selectedHour + ":" + selectedMinute;
+                STOP_TIME_HH = selectedHour;
+                STOP_TIME_MM = selectedMinute;
                 TextView stopTime = (TextView)findViewById(R.id.stopTime);
                 stopTime.setText(STOP_TIME);
             }
         }, hour, minute, true);
         mTimePicker.setTitle("Select Stopping Time");
         mTimePicker.show();
+    }
+
+    public void setStartTimeConfig(View v){
+        alarmMgr = (AlarmManager)getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(getApplicationContext(), TheaterOn.class);
+        alarmIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
+
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, START_TIME_HH);
+        calendar.set(Calendar.MINUTE, START_TIME_MM);
+
+
+        alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY, alarmIntent);
     }
     //config
 
@@ -200,8 +232,7 @@ public class PhoneConfig extends AppCompatActivity implements GoogleApiClient.Co
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Toast.makeText(getApplicationContext(),                     "Error, not connected to phone!", Toast.LENGTH_SHORT).show();
     }
-
-public void CallTheaterOn(){
+public final void TimerSendOn(){
     if (wearNode != null && wearGoogleApiClient!=null && wearGoogleApiClient.isConnected()) {
         //
         Wearable.MessageApi.sendMessage(
@@ -226,35 +257,7 @@ public void CallTheaterOn(){
 
     }
 
+}
 
 }
 
-    public void CallTheaterOff(){
-        if (wearNode != null && wearGoogleApiClient!=null && wearGoogleApiClient.isConnected()) {
-            //
-            Wearable.MessageApi.sendMessage(
-                    wearGoogleApiClient, wearNode.getId(), on, null).setResultCallback(
-
-                    new ResultCallback<MessageApi.SendMessageResult>() {
-                        @Override
-                        public void onResult(MessageApi.SendMessageResult sendMessageResult) {
-
-                            if (!sendMessageResult.getStatus().isSuccess()) {
-                                Log.e("TAG", "Failed to send message with status code: "
-                                        + sendMessageResult.getStatus().getStatusCode());
-                            }
-                        }
-                    }
-            );
-        }else{
-            Toast.makeText(getApplicationContext(),
-                    "No connection to phone", Toast.LENGTH_LONG).show();
-            Toast.makeText(getApplicationContext(),
-                    "Connect watch to phone!", Toast.LENGTH_SHORT).show();
-
-        }
-
-
-    }
-
-}
--- mobile/src/main/java/com/chernowii/theatertime/TheaterOn.java
+++ mobile/src/main/java/com/chernowii/theatertime/TheaterOn.java
@@ -9,13 +9,12 @@ import android.widget.Toast;
  * Created by Konrad Iturbe on 02/06/16.
  */
 public class TheaterOn extends BroadcastReceiver{
-    public PhoneConfig phone;
     @Override
-    public void onReceive(Context _context, Intent _intent) {
-        //PhoneConfig.sendOn();
-        phone.CallTheaterOn();
-        Toast.makeText(_context, "Theater ON", Toast.LENGTH_LONG).show();
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Theater ON", Toast.LENGTH_LONG).show();
     }
 
 
 }
+
+
